de3801a03c0d45d55d45045175b6cf3e95915b40
Oscar <oscar.garcia_telles@hotmail.com>
2016-06-27 17:56:02 -0700

    First commit

---
+++ Stuff.java
@@ -0,0 +1,11 @@
+/**
+ * Created by oscar on 6/27/16.
+ */
+public class Stuff {
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello, world!!!");
+    }
+}
---
+++ src/Shell.java
@@ -0,0 +1,21 @@
+/**
+ * @author !@#$%%$#@!!@#$%%$#@!      PUT YOUR NAME HERE !@#$%%$#@!!@#$%%$#@!!@#$%%$#@!
+ *
+ * There's a separate folder with the ThreadOS .class files
+ *
+ */
+
+public class Shell extends Thread {
+
+    public Shell()  {
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Shell shell = new Shell();
+        shell.run();
+    }
+}
